de94d5b7d98773bee4c6a838bc0deae9e7af3632
cgonmat0908b <cgonmat0908b@g.educaand.es>
2025-12-01 21:23:30 +0100

    Ejs

--- Gonzalez_Mateo_Cristian_5_16/src/ejercicio1/Ejercicio1.java
+++ Gonzalez_Mateo_Cristian_5_16/src/ejercicio1/Ejercicio1.java
@@ -2,7 +2,8 @@ package ejercicio1;
 
 import libtarea3.CuentaBancaria;
 import java.time.LocalDate;
-import java.lang.Exception;
+import java.time.DateTimeException;
+import java.lang.IllegalArgumentException;
 
 public class Ejercicio1 {
 
@@ -13,12 +14,12 @@ public class Ejercicio1 {
 		//USAR PRINTF EN TODOS LOS CASOS POSIBLES 
 		
 		
-		//1.1
+		//Ejercicio 1
 		
 		//java.lang.IllegalArgumentException
 		
 		
-		//Apartado 1
+		//Apartado 1 Declaración de 3 cuentas
 		CuentaBancaria cuentaPrivada;
 		CuentaBancaria cuentaConjunta;
 		CuentaBancaria cuentaFamiliar;
@@ -27,16 +28,14 @@ public class Ejercicio1 {
 		
 		//Apartado 2 
 		
-		cuentaPrivada = new CuentaBancaria();
-		cuentaConjunta = new CuentaBancaria();
-		cuentaFamiliar = new CuentaBancaria();
-		
-		
 		//Apartado 2.1, Creación de cuenta con fecha erronea resto de valores validos
 		
 		try {
 			LocalDate fechaMal = LocalDate.of(2027,9,1);
 			CuentaBancaria cuenta = new CuentaBancaria (0.00, fechaMal);
+			
+		}catch(DateTimeException exfechaNoValida) {
+			System.out.println(exfechaNoValida.getMessage());
 			
 		}catch(IllegalArgumentException exFecha){
 			System.out.println(exFecha.getMessage());
@@ -46,6 +45,7 @@ public class Ejercicio1 {
 		//Apartado 2.2 Creación de fecha con cantidad de dinero invalida resto de valores validos
 		
 		try {
+			
 			CuentaBancaria saldoErroneo = new CuentaBancaria(-200.00);
 			
 		}catch(IllegalArgumentException exSaldo){
@@ -53,21 +53,67 @@ public class Ejercicio1 {
 		}
 		
 		
-		//Apartado 2.3
+		//Apartado 2.3 Creacion de 3 cuentas válidas, Las cuales apuntan a cuentas declaradas  en el apartado 1
+		
 		LocalDate fechaBien = LocalDate.of(2021, 7, 1);
-		CuentaBancaria descubiertoNegativo = new CuentaBancaria(1000.00, fechaBien, -200.00);
+		cuentaPrivada = new CuentaBancaria(1000.00, fechaBien, -200.00);
 		
-		cuentaPrivada = descubiertoNegativo;
+		cuentaConjunta = new CuentaBancaria(200, fechaBien);
 		
-		CuentaBancaria sinDescubierto = new CuentaBancaria(200, fechaBien);
-		cuentaConjunta = sinDescubierto;
+		cuentaFamiliar = new CuentaBancaria();
 		
-		CuentaBancaria cuenta = new CuentaBancaria();
+		//Apartado 3 Mostrar información de cuentaPrivada.
 		
+		//Apartado 3.1 Identificador de la cuenta
+		System.out.println();
+		System.out.printf("El identificador de la cuenta bancaria es %d ", cuentaPrivada.getId());
+		System.out.println();
 		
-		//Apartado 1.2
+		//Apartado 3.2 Fecha de creación de la cuenta
+		String fechaCuenta = fechaBien.toString();
+		System.out.printf("La fecha de creación de la cuenta es %s", fechaCuenta);
+		System.out.println();
 		
-		//Apartado 1.3
+		//Apartado 3.3 Limite de descubierto con 2 decimales
+		System.out.printf("El limite de descubierto es: %.2f",cuentaPrivada.getLimiteDescubierto());
+		System.out.println();
+		
+		//Apartado 3.4 Cuenta embargada?
+		System.out.printf("Esta embargada? %b", cuentaPrivada.isEmbargada());
+		System.out.println();
+		
+		//Apartado 3.5 Cuenta descubierta?
+		System.out.printf("Esta descubierta? %b", cuentaPrivada.isDescubierta());
+		System.out.println();
+		
+		//Apartado 3.6 Longevidad de la cuenta
+		System.out.printf("La cuenta lleva abierta %d dias", cuentaPrivada.getDiasCuenta());
+		System.out.println();
+		//Apartado 4 Operaciones con la cuenta
+		
+		//Apartado 4.1 Ingreso en cuentaFamiliar
+		cuentaFamiliar.ingresar(100.00);
+		
+		//Apartado 4.2 Extracción en cuentaConjunta
+		cuentaConjunta.extraer(100.00);
+		
+		//Apartado 4.3 Transferencia de privada a familiar
+		cuentaPrivada.transferir(1100.00, cuentaFamiliar);
+		
+		//Apartado 5 Estado de las cuentas
+		
+		//Apartado 5.1 Estado CuentaPrivada
+		System.out.println();
+		System.out.printf("Datos de la cuenta privada: %s", cuentaPrivada.toString());
+		System.out.println();
+		
+		//Apartado 5.2 Estado CuentaConjunta
+		System.out.printf("Datos de la cuenta conjunta: %s", cuentaConjunta.toString());
+		System.out.println();
+		
+		//Apartado 5.3 Estado CuentaFamiliar
+		System.out.printf("Datos de la cuenta familiar: %s", cuentaFamiliar.toString());
+		
 	}
 
 }
--- Gonzalez_Mateo_Cristian_5_16/src/ejercicio2/Ejercicio2.java
+++ Gonzalez_Mateo_Cristian_5_16/src/ejercicio2/Ejercicio2.java
@@ -1,10 +1,42 @@
 package ejercicio2;
 
+import libtarea3.Dado;
+
+import java.lang.IllegalArgumentException;
+
 public class Ejercicio2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		//Apartado 1 Consulta Inicial valores clase dado
+		
+		//Apartado 1.1 Cantidad dados creados
+		System.out.printf("Numero de dados %d", Dado.getNumeroDadosCreados());
+		System.out.println();
+		
+		//Apartado 1.2 Numero de lanzamientos
+		System.out.printf("Numero de lanzamientos %d", Dado.getNumeroLanzamientosGlobal());
+		System.out.println();
+		
+		//Apartado 1.3 Veces que ha salido 1,2,3 y 4 en total
+		int numTotal = Dado.getNumeroVecesCaraGlobal(1) + Dado.getNumeroVecesCaraGlobal(2) + Dado.getNumeroVecesCaraGlobal(3) + Dado.getNumeroVecesCaraGlobal(4) ;
+		System.out.printf("Veces totales que han salido 1,2,3 y 4 es: %d", numTotal);
+		System.out.println();
+		
+		//Apartado 2 Creación y lanzamiento de dado
+		
+		//Apartado 2.1 Creacion
+		try {
+			for(int i = 0; i < 10; i++) {
+				Dado dadoCreado = new Dado((int)(Math.random() * 8));
+			}
+			
+			
+			
+		}catch(IllegalArgumentException exDado) {
+			System.out.printf("Numero de caras invalido %s", exDado.toString());
+		}
+		
 	}
 
 }
