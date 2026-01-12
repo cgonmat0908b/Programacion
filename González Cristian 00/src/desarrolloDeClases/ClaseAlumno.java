a6015f9bdc7c0ca584df564978a3efeb4f273bd9
cgonmat0908b <cgonmat0908b@g.eduacaand.es>
2026-01-12 12:38:07 +0100

    Continuación clase Alumno

--- González Cristian 00/src/desarrolloDeClases/ClaseAlumno.java
+++ González Cristian 00/src/desarrolloDeClases/ClaseAlumno.java
@@ -1,8 +1,15 @@
+/* Descripción: Creación de la clase Alumno con los atributos indicados, como atributos para el constructor, se usará
+ * los atributos que considere relevantes. 
+ * Autor: Cristian González Mateo
+ * Fecha: 12/01/2025
+ */
+
 package desarrolloDeClases;
 
 import java.time.LocalDate;
-
+import java.lang.IllegalArgumentException;
 import java.time.LocalTime;
+
 
 public class ClaseAlumno {
 
@@ -29,7 +36,10 @@ public class ClaseAlumno {
 	 * - Asignar si es un atributo de objeto o de clase
 	 */
 	
+	// Si tuvieramos un tipo Enum se pondria por encima de los atributos de clase constantes
+	
 	// Atributos de clase constantes
+	
 	public static final byte MAX_ALUMNOS_GRUPO = 30;
 	
 	public static final LocalTime HORA_CENTRO = LocalTime.of(21,30);
@@ -38,10 +48,8 @@ public class ClaseAlumno {
 	
 	public static final byte NUM_MIN_ALUM_GRUPO = 1;
 	
-	public static final LocalTime HORA_MAX_ALUM = LocalTime.of(15,00);
-	
-	
 	// Atributos de clase variables
+	
 	private static int numAlumnosMatriculados;
 	
 	// Atributos de objeto variables
@@ -52,7 +60,7 @@ public class ClaseAlumno {
 	
 	private String apellidos;
 	
-	private short peso;
+	private float peso;
 	
 	private float altura;
 	
@@ -62,20 +70,61 @@ public class ClaseAlumno {
 	
 	private boolean mayorEdad;	
 	
+	private LocalTime horaMaxAlum;
+	
 	/*
-	 * Creación del contructor de la clase ClaseAlumno
+	 * Creación del constructor de la clase ClaseAlumno
 	 */
 	
-	public ClaseAlumno(String dni,String nombre,String apellidos,short peso,float altura,LocalDate fechaNacimiento,byte numHermanos,boolean mayorEdad) {
+	public ClaseAlumno(String dni,String nombre,String apellidos,float peso,float altura,LocalDate fechaNacimiento,byte numHermanos,boolean mayorEdad, LocalTime horaMaxAlum) throws IllegalArgumentException{
 		
-		if(DniValido(dni) == false) {
+		if(dni == null || dni.isEmpty()) {
+			throw new IllegalArgumentException("El parametro es nulo o contiene una cadena vacia");
 			
+		}else if(DniFormato(dni) == false) {
+			throw new IllegalArgumentException("El formato del dni es incorrecto");
+			
+		}else if(DniCorrecto(dni) == false) {
+			throw new IllegalArgumentException("El dni introducido no es correcto");
 		}
 		
+		if(nombre == null || nombre.isEmpty()) {
+			throw new IllegalArgumentException("El parametro es nulo o contiene una cadena vacia");
+		}
+		
+		if(apellidos == null || apellidos.isEmpty()) {
+			throw new IllegalArgumentException("El parametro es nulo o contiene una cadena vacia");
+		}
+		
+		if(peso <= 0) {
+			throw new IllegalArgumentException("El peso no puede ser menor o igual a 0");
+		}
+		if(altura <= 0) {
+			throw new IllegalArgumentException("La altura no puede ser menor o igual a 0");
+		}
+		if(FechaNacimCorrecta(fechaNacimiento) == false) {
+			throw new IllegalArgumentException("La fecha introducida no es correcta");
+		}
+		if(numHermanos < 0) {
+			throw new IllegalArgumentException("No puedes tener hermanos negativos");
+		}
+		
+		this.dni = dni;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.peso = peso;
+		this.altura = altura;
+		this.fechaNacimiento = fechaNacimiento;
+		this.numHermanos = numHermanos;
+		this.mayorEdad = MayorEdad(fechaNacimiento);
+		
+		numAlumnosMatriculados++;
 		
 	}
 	
-	public static boolean DniValido(String dni) {
+	//Metodo que comprueba que el formato del DNI sea 8 numeros y 1 letra dado el Dni como parametro.
+	
+	public static boolean DniFormato(String dni) {
 		boolean dniValido = false;
 		
 		if(dni == null || !dni.matches("[0-9]{8}[A-Z]")) {
@@ -87,6 +136,65 @@ public class ClaseAlumno {
 		}
 		return dniValido;
 	}
+	
+	// Método que comprueba que los números del DNI corresponden con la letra dado el DNI como parametro.
+	
+	public static boolean DniCorrecto(String dni) {
+		
+		boolean dniCorrecto = false;
+		char[] letras = {
+		        'T','R','W','A','G','M','Y','F','P','D','X',
+		        'B','N','J','Z','S','Q','V','H','L','C','K','E'
+		    };
+		
+		char letraDni = dni.charAt(dni.length()-1);
+		int numero = Integer.parseInt(dni.substring(0, dni.length() - 1));
+		int modulo = numero % 23;
+		
+		if(letraDni == letras[modulo]) {
+			dniCorrecto = true;
+		}
+		
+		return dniCorrecto;
+	}
+	
+	// Método que comprueba que la fecha de nacimiento se encuentra entre unos valores determinados. 
+	
+	public static boolean FechaNacimCorrecta(LocalDate fechaNacimiento) {
+		boolean fechaCorrecta = false;
+		
+		if(fechaNacimiento.isAfter(LocalDate.now()) == true || fechaNacimiento.isBefore(LocalDate.of(1900, 01, 01 ))) { 
+			fechaCorrecta = false;
+			
+		}else {
+			fechaCorrecta = true;
+		}
+		
+		return fechaCorrecta;
+	}
+	
+	//Método que comprueba si el alumno es mayor de edad dada su fecha de nacimiento.
+	
+	public static boolean MayorEdad(LocalDate fechaNacimiento) {
+		boolean mayor = false;
+		int edad = (LocalDate.now().getYear()) - fechaNacimiento.getYear();
+		
+		if(edad >= 18) {
+			mayor = true;
+		}
+		
+		return mayor;
+		
+	}
+	
+	/*Dudas: 
+	 * 
+	 * MayoriaEdad: La mayoria de edad he realizado un método para calcularla, no estoy seguro de si en el constructor se ha de introducir como parametro/atributo.
+	 * 
+	 * LocalTime horaMaxAlum, no he echo con el atributo nada aún porque pienso en añadir como atributo turnoAlumno y asignar la horaMaxAlum según el turno que tenga como parámetro.
+	 * 
+	 * 
+	 */
 }
 
 
